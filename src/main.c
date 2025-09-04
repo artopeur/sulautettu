38dc86073b4f48df20bbad1d9b06d64aa641eae2
artopeur <arto.peurasaari@gmail.com>
2025-09-04 15:54:41 +0300

    tuntiesimerkki.

--- src/main.c
+++ src/main.c
@@ -1,48 +1,57 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+// *******************************
+// This example declares two tasks
+// and runs them in parallel
 
-#include <stdio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+// Task declarations
+void task1(void *, void *, void*);
+void task2(void *, void *, void*);
+#define	STACKSIZE	500
+#define	PRIORITY	5
+K_THREAD_DEFINE(tid1,STACKSIZE,task1,NULL,NULL,NULL,PRIORITY,0,0);
+K_THREAD_DEFINE(tid2,STACKSIZE,task2,NULL,NULL,NULL,PRIORITY,0,0);
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+int tila = 0;
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-int main(void)
-{
-	int ret;
-	bool led_state = true;
-
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
+// Main program
+int main(void) {
+	/*
+	while (true) {
+		printk("Hello from main\n");
+		k_msleep(1000);
+		// k_yield();
 	}
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
-
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
-	}
+	*/
+	tila = 1;
 	return 0;
+}
+
+// Task1 function
+void task1(void *, void *, void*) {
+	while (true) {
+		if(tila == 1) {
+			printk("Hello from task 1\n");
+			
+			tila = 2;
+		}
+		//k_msleep(3000);
+		k_yield();
+	}
+}
+
+// Task2 function
+void task2(void *, void *, void*) {
+
+	while (true) {
+		if(tila == 2) {
+			printk("Hello from task 2\n");
+			
+			
+			tila = 1;
+		}
+		//k_msleep(5000);
+		k_yield();
+	}
 }
